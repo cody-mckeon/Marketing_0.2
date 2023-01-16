d07ebe7e865e9c41f3524693e915b0125e01ea98
Cody McKeon <codymckeon@gmail.com>
2023-01-15 19:38:57 -0700

    bootstrapped

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,22 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import PostScreen from './screens/PostScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/"> The Campaigner </Link>
+          <Navbar bg="light" variant="light">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>The Campaigner</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
@@ -16,6 +25,9 @@ function App() {
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
